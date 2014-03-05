9fb511e8742a0ff15c41ebfa37cf21c70ad99e32
zapree <epbalaguer@gmail.com>
2014-03-04 22:19:42 -0700

    changes to jscrpt
    modified the index back, changed js and removefromcart

--- js/main_script.js
+++ js/main_script.js
@@ -15,16 +15,21 @@
 	}
 	
 	Cart.prototype.addcartitem = function(item) {
-		
+		//for adding additional amounts of the same item
 		if( item.product in this.item_list){
 			item.quantity ++;
 		}
-		
+		//if item isn't aready in the list, add it
 		else{
 			this.item_list[item.product] = item;
 			item.quantity = 1;
 		}
 			
+	};
+	
+	Cart.prototype.removecartitem = function(item) {
+		//get rid of the element from the cart list
+		delete this.item_list[item.product];
 	};
 	
 	Cart.prototype.getcartinfo = function() {
@@ -36,3 +41,18 @@
 		}
 		
 	};
+	/*
+	function cartline(){
+		product
+		price
+		quantity
+		line_total
+		<tr>
+					<td>Super Awesome Island Yacht</td>
+					<td>$1,000,000,000.00</td>
+					<td>1</td>
+					<td>$1,000,000,000.00</td>
+					<td><a href="">remove?</a></td>
+				</tr>
+	}
+	*/
