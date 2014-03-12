9ced95bcd9e18996fd2ecf7ca264f8d287de14c3
zapree <epbalaguer@gmail.com>
2014-03-11 17:22:41 -0700

    cart in final stages
    still a bug with the cart not getting rid of the last entry sometimes.

--- js/main_script.js
+++ js/main_script.js
@@ -28,6 +28,7 @@
 		else{
 			this.item_list[item.product] = item;
 			item.quantity = 1;
+			item.line_total = item.quantity*item.price;
 		}
 		
 		this.cart_total += item.price;
@@ -36,8 +37,9 @@
 	
 	Cart.prototype.removecartitem = function(item) {
 		//get rid of the element from the cart list
+		this.cart_total -= item.line_total;
 		delete this.item_list[item.product];
-		item.cart_total -= item.line_total;
+		
 	};
 	
 	Cart.prototype.getcartinfo = function() {
@@ -52,28 +54,41 @@
 	
 	function CartTable(the_cart){
 		this.table = document.getElementById("cart_table");
-		this.total_cell = document.getElementById("total_cell");
 		this.row_list = {};
+		this.cart = the_cart;
+		
+		//create header fields
 		header = this.table.createTHead();
 		header_row = header.insertRow();
-		footer = this.table.createTFoot();
-		footer_row = footer.insertRow(-1);
 		cell_list = ["Product", "Price", "Quantity", "Line Total"];
 		for(cell in cell_list){
 			current_cell = header_row.insertCell(-1);
 			current_cell.innerHTML = cell_list[cell];
-			console.log(cell_list[cell]);
 		}
+		//filler line after header
+		filler = this.table.insertRow(-1);
+		filler.setAttribute("colspan", "5");
+		filler.setAttribute("class", "filler_row");
+		filler.setAttribute("required", "true");
+		
+		//add the items to the cart
 		for(item in the_cart.item_list){
 			this.additemrow(the_cart.item_list[item]);
 		}
-		cell_list = ['','','Total','$0',]
+		//filler before footer
+		filler = this.table.insertRow(-1);
+		filler.colspan="5";
+		filler.setAttribute("class", "filler_row");
+		//create footer with the cart total
+		cell_list = ['','','Total'];
+		footer = this.table.createTFoot();
+		footer_row = footer.insertRow(-1);
 		for(cell in cell_list){
 			current_cell = footer_row.insertCell(-1);
 			current_cell.innerHTML = cell_list[cell];
-			console.log(cell_list[cell]);
 		}
-		this.updatetotal(the_cart);
+		this.total_cell = footer_row.insertCell(-1);
+		this.updatetotal();
 		}
 	
 	CartTable.prototype.additemrow = function(item) {
@@ -84,44 +99,43 @@
 		cell_list = [item.product, item.price, item.quantity, item.line_total];
 		for(cell in cell_list){
 			current_cell = row.insertCell(-1);
-			current_cell.innerHTML = cell_list[cell];
-			console.log(cell_list[cell]);
+			if( cell != 1 && cell != 3){
+				current_cell.innerHTML = cell_list[cell];
+			}
+			else current_cell.innerHTML = "$" +cell_list[cell].toString().replace( /(\d)(?=(\d{3})+$)/g, "$1,")+".00";
 		}
+		current_cell = row.insertCell(-1);
+		var butt = document.createElement('input'); // create a button
+	    butt.setAttribute('type','button'); // set attributes ...
+	    butt.setAttribute('name','javaSubmit');
+	    butt.setAttribute('value','Remove');
+	    that = this;
+	    butt.onclick = function() {
+	    	that.deleterow(item);
+	    };
+	    row.cells[4].appendChild(butt);
+		
 	};
-	
 	CartTable.prototype.deleterow = function(item) {
-		this.row_list[item.product].delete
+		console.log("trying to delete");
+		num =1;
+		for(var key in this.row_list)
+		{
+			num++;
+			
+		    if(this.row_list[key]==this.row_list[item.product])
+		         break;
+		}
+		console.log(this.table);
+		this.table.deleteRow(num);
+		this.cart.removecartitem(item);
+		this.updatetotal();
 	};
 	
-	CartTable.prototype.updatetotal = function(passed_cart){
-		this.total_cell.innerHTML = passed_cart.cart_total;
+	CartTable.prototype.updatetotal = function(){
+		this.total_cell.innerHTML = "$"+this.cart.cart_total.toString().replace( /(\d)(?=(\d{3})+$)/g, "$1,"  )+".00";
 	};
-	
-	
-	/*
-				<tr><td colspan="5"><hr></td></tr>
-				<tr>
-					<td>Super Awesome Island Yacht</td>
-					<td>$1,000,000,000.00</td>
-					<td>1</td>
-					<td>$1,000,000,000.00</td>
-					<td><a href="">remove?</a></td>
-				</tr>
-				<tr>
-					<td>Super Awesome Car</td>
-					<td>$150,000.00</td>
-					<td>2</td>
-					<td>$300,000.00</td>
-					<td><a href="">remove?</a></td>
-				</tr>
-				<tr>
-					<td>Super Awesome Tee</td>
-					<td>$149.00</td>
-					<td>1</td>
-					<td>$149.00</td>
-					<td><a href="">remove?</a></td>
-				</tr>
-	*/			
+		
 	
 
 
