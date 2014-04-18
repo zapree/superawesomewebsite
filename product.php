5ef5dd21a0cb35e61df9f73fd5940205379e5c56
Eugene Balaguer <epbalaguer@email.arizona.edu>
2014-04-17 19:48:52 -0700

    updated product
    updated products to get the right product from the url

--- product.php
+++ product.php
@@ -1,48 +1,11 @@
 <?php $title = "Product Title from DB"//some DB call ?>
-<?php include('header.php'); ?>
-<h3>
-	You Heard Right, A Yacht Island
-</h3>
-<div class="yacht_and_cart">
-	<img id="product_yacht" alt ="superyacht" src="pictures/goddamnislandyacht.jpg"/>
-	<form id="addtocart" action="cart.php" method="get">
-		<fieldset>
-			<legend>Add to Cart</legend>
-			<h3>$1,000,000,000</h3>
-			<input type="text" name="productquantity" value="1" id="productquantity"/>
-			<input type="submit"/>
-		</fieldset>
-	</form>
-</div>
+<?php include('header.php'); 
 
-<p>
-	Are you a mega rich investor with not enough 
-	frivilous things to sink your money into?  Well buy yourself a yacht Island.
-	They are the pinnacle of everyone knowing you have a ton of gold to your name.
-	Buy one of these babies and you're guaranteed to get a harem of girls, and you can fit
-	like 50 people on this thing.  Imagine 49 smokin hot naked girls at your beck and call, and hey,
-	it's international waters.
-</p>	
-<p>
-	Since I don't really have much more to say about this here are the lyrics to pink floyd's
-	wish you were here. So, so you think you can tell Heaven from Hell, blue skies from pain.
-	Can you tell a green field from a cold steel rail?
-	A smile from a veil?
-	Do you think you can tell?
-</p>	
-<p>
-	Did they get you to trade your heroes for ghosts?
-	Hot ashes for trees?
-	Hot air for a cool breeze?
-	Cold comfort for change?
-	Did you exchange a walk on part in the war for a lead role in a cage?
-	
-	How I wish, how I wish you were here.
-	We're just two lost souls swimming in a fish bowl, year after year,
-	Running over the same old ground.
-	What have we found?
-	The same old fears.
-	Wish you were here.
-</p>
+$productName = $_GET['product'];
+$DBH = new PDO("mysql:host=localhost;dbname=hw4", 'root', '');
+$stmt = $DBH->prepare("SELECT description FROM product WHERE id = $productName");
+$stmt->execute(); 
+$product_html = $stmt -> fetch();
+echo $product_html[0];
 
-<?php include('footer.php'); ?>
+include('footer.php'); ?>
