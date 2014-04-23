0e4eb1d9e57c67f101c46b45073fb7c1e9a8644f
Eugene Balaguer <epbalaguer@email.arizona.edu>
2014-04-22 17:43:33 -0700

    Product page update
    Updated the product page.

--- product.php
+++ product.php
@@ -11,10 +11,36 @@ $cleanProduct = filter_var($productName, FILTER_SANITIZE_STRING);
 $cleanProduct = htmlentities($cleanProduct);
 
 $DBH = new PDO("mysql:host=localhost;dbname=hw4", 'root', '');
-$stmt = $DBH->prepare("SELECT description FROM product WHERE id = $cleanProduct");
+$stmt = $DBH->prepare("SELECT * FROM product WHERE id = $cleanProduct");
 $stmt->bindValue(':atitle', $cleanProduct, PDO::PARAM_STR);
 $stmt->execute(); 
 $product_html = $stmt -> fetch();
-echo $product_html[0];
+?>
+
+
+
+<h3>
+	You Heard Right, <?php echo $product_html['name'] ?>
+</h3>
+<div class="yacht_and_cart">
+	<img id="product_yacht" alt ="<?php echo $product_html['name'] ?>" src="<?php echo $product_html['image'] ?>"/>
+	<form id="addtocart" action="cart.php" method="get">
+		<fieldset>
+			<legend id="money">Add to Cart</legend>
+			<script>
+				var money = <?php echo $product_html['price'] ?>;
+				money = money.toString().replace( /(\d)(?=(\d{3})+$)/g, "$1,");
+				console.log($(this).parent());
+				$("<h3>$"+money+".00</h3>").insertAfter($("#money"));
+			</script>
+			
+			<input type="text" name="productquantity" value="1" id="productquantity"/>
+			<input type="submit"/>
+		</fieldset>
+	</form>
+</div>
+<?php 
+
+echo $product_html['description'];
 
 include('footer.php'); ?>
